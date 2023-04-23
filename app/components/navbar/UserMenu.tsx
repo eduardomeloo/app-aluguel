9e6aaaffebcd9b65750eec9090e265555f4d45ad
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-04-22 22:46:27 -0300

    create and configure component MenuItem

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,13 +1,20 @@
 'use client'
 import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar';
+import { useState, useCallback } from 'react';
+import MenuItem from './MenuItem';
 const UserMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    }, []);
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
                 <div    onClick={() => {}} 
                         className="
-                            hiddem
+                            hidden
                             md:block
                             text-sm
                             font-semibold
@@ -21,7 +28,7 @@ const UserMenu = () => {
                 >
                     Anuncie seu espaço
                 </div>
-                <div    onClick={() => {}} 
+                <div    onClick={toggleOpen} 
                         className="
                             p-4
                             md:py-1
@@ -44,6 +51,29 @@ const UserMenu = () => {
                     </div>
                 </div>
             </div>
+            {isOpen && (
+                <div 
+                    className="
+                        absolute
+                        rounded-xl
+                        shadow-md
+                        w-[40vw]
+                        md:w-3/4
+                        bg-white
+                        overflow-hidden
+                        right-0
+                        top-12
+                        text-sm
+                    "
+                >
+                    <div className="flex flex-col cursor-pointer">
+                        <>
+                            <MenuItem onClick={() => {}} label='Login' />
+                            <MenuItem onClick={() => {}} label='Sign up' />
+                        </>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
