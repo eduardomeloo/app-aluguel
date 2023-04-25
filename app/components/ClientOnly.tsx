827a70f15e9f85f2238a411826df53a30e365d03
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-04-24 21:34:26 -0300

    create component ClientOnly

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+interface ClientOnlyProps {
+    children: React.ReactNode;
+}
+
+const ClientOnly: React.FC<ClientOnlyProps> = ({children}) => {
+    const [hasMouted, setHasMounted] = useState(false);
+
+    useEffect(() => {
+        setHasMounted(true);
+    },[hasMouted]);
+
+    if (!hasMouted) return null;
+
+    return ( 
+        <>
+            {children}
+        </>
+    );
+}
+ 
+export default ClientOnly;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
 
 export const metadata = {
   title: 'App Aluguel',
   description: 'Procurando um lugar para chamar de lar? Temos a solução perfeita para você! Venha conferir nossas opções de imóveis para aluguel e encontre o seu novo lar dos sonhos.',
 }
 
-const font = Nunito({
-    subsets: ["latin"]
-});
+const font = Nunito({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -19,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
         <body className={font.className}>
-            <Navbar />
+            <ClientOnly>
+                <Navbar />
+            </ClientOnly>
             {children}
         </body>
     </html>
