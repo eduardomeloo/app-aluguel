e41749834e684cd10cee3ff1477b4f8336f5bf24
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-04-27 21:55:53 -0300

    install react-hook-form

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,4 +1,11 @@
-'use client'
+'use client';
+
+import axios from "axios";
+import { AiFillGithub } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import userRegisterModal from "@/app/hooks/useRegisterModal";
 
 const RegisterModal = () => {
     return (
