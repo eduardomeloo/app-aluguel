b7743c1be8e8604552177fb2fd35980608003d92
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-04-27 22:16:50 -0300

    configure component RegisterModal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -6,12 +6,44 @@ import { FcGoogle } from 'react-icons/fc';
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import userRegisterModal from "@/app/hooks/useRegisterModal";
+import Modal from "./Modal";
 
 const RegisterModal = () => {
-    return (
-        <div className="">
+    const registerModal = userRegisterModal();
+    const [isLoading, setIsLoading] = useState(false);
 
-        </div>
+    const { register, handleSubmit, formState: { errors }} = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        axios.post('/api/register', data)
+            .then(() => {
+                registerModal.onClose();
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }
+
+    return (
+        <Modal
+            disabled={isLoading}
+            isOpen={registerModal.isOpen}
+            title="Register"
+            actionLabel="Continue"
+            onClose={registerModal.onClose}
+            onSubmit={handleSubmit(onSubmit)}
+        />
     );
 }
 
