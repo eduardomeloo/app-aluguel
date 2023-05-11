e980cca21c287c942a57dd1f1a62fecd408957f5
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-05-10 22:15:50 -0300

    create file getCurrentUser

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import ClientOnly from './components/ClientOnly';
 import RegisterModal from './components/modals/RegisterModal';
 import ToasterProvider from './providers/ToasterProvider';
 import LoginModal from './components/modals/LoginModal';
+import getCurrentUser from './actions/getCurrentUser';
 
 export const metadata = {
   title: 'App Aluguel',
@@ -13,22 +14,24 @@ export const metadata = {
 
 const font = Nunito({ subsets: ["latin"] });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-        <body className={font.className}>
-            <ClientOnly>
-                <ToasterProvider />
-                <LoginModal />
-                <RegisterModal />
-                <Navbar />
-            </ClientOnly>
-            {children}
-        </body>
-    </html>
-  )
+    const currentUser = await getCurrentUser();
+
+    return (
+        <html lang="en">
+            <body className={font.className}>
+                <ClientOnly>
+                    <ToasterProvider />
+                    <LoginModal />
+                    <RegisterModal />
+                    <Navbar />
+                </ClientOnly>
+                {children}
+            </body>
+        </html>
+    )
 }
