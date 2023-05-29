7431cb91da06b92788b56e31e4400041f5e78ba1
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-05-28 22:28:47 -0300

    create component CategoryInput

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -5,6 +5,7 @@ import Modal from "./Modal";
 import useRentModal from "@/app/hooks/useRentModal";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
 
 enum STEPS {
     CATEGORY    = 0,
@@ -56,7 +57,12 @@ const RentModal = () => {
                 ">
                 {categories.map((item) => (
                     <div key={item.label} className="col-span-1">
-                        {item.label}
+                        <CategoryInput 
+                            onClick={() => {}}
+                            selected={false}
+                            label={item.label}
+                            icon={item.icon}
+                        />
                     </div>
                 ))}
             </div>
